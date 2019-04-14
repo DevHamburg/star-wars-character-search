c9a8b34f0ee859ce76fb9506aa0e1d1d70431a17
DevHamburg <guberinic040@gmail.com>
2019-04-14 19:12:29 +0200

    Deployment version

--- server/server.js
+++ server/server.js
@@ -3,10 +3,18 @@ const app = express()
 const bodyParser = require('body-parser')
 const fs = require('fs')
 const cors = require('cors')
+const path = require('path')
 
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cors())
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(___dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.get('/all', (req, res) => {
   const data = fs.readFileSync('search-data.txt', 'utf-8')
